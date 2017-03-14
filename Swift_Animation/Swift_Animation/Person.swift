20afef1165f0dbf24798ca327f72fdb9fa9d141e
lul <630001384@qq.com>
2017-03-14 09:47:16 +0800

    test

---
+++ Swift_Animation/Swift_Animation/Person.swift
@@ -0,0 +1,25 @@
+//
+//  Person.swift
+//  Swift_Animation
+//
+//  Created by 陈璐 on 2017/3/13.
+//  Copyright © 2017年 Chenlu. All rights reserved.
+//
+
+import UIKit
+/*
+ 必选属性的构造过程：
+ 1、给自己的属性分配空间并设置初始值
+ 2、调用父类的构造函数，给父类的属性分配空间并设置初始值
+ 以上这两个顺序和OC相比是完全相反的，OC中是先调用父类的方法，然后给自己的属性分配空间并设置初始值。
+ var name: String
+ //override表示重写
+ override init() {
+ name = "zhang"
+ super.init()
+ }
+ */
+
+class Person: NSObject {
+
+}
--- Swift_Animation/Swift_Animation/ViewController.swift
+++ Swift_Animation/Swift_Animation/ViewController.swift
@@ -13,13 +13,90 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+//        demo()
+        
+//        print("\(sum(one: 3, two: 6))");
+        
+//        print("\(sum2(2, 7))")
+        
+        
+//        let b = {
+//            (x: Int) ->() in
+//            print(x);
+//        }
+//        b(100);
+
+        
+//        let b3 = {
+//            (x: Int) -> Int in
+//            return x + 10;
+//        }
+//        print(b3(55))
+        
+//        DispatchQueue.global().async {
+//            print("耗时操作\(Thread.current)")
+//            sleep(3)
+//            DispatchQueue.main.async {
+//                print("主线程更新UI\(Thread.current)")
+//            }
+//        }
+        
+        loadData { (result) in
+            print("this is result\(result)")
+        }
+        
+        
     }
 
+    
+    func loadData(completion: @escaping (_ result: [String]) -> ()) -> () {
+        DispatchQueue.global().async {
+            print("sleep")
+            Thread.sleep(forTimeInterval: 3)
+            let json = ["aaaa","bbb","cccccccc"]
+            DispatchQueue.main.async {
+                print("main load UI")
+                completion(json)
+            }
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+//    func demo()  {
+//        let dict = ["a":1,"b":2];
+////        print(dict)
+//
+//        let array: [[String: AnyObject]] = [
+//            ["name":"张" as AnyObject,"age":"12" as AnyObject],
+//            ["name":"张" as AnyObject,"age":"12" as AnyObject]
+//        ]
+////        print(array);
+//        
+//        
+////        for apple in dict {
+////            print("\(apple.key)\(apple.value)")
+////        }
+//
+//        for (k,z) in dict {
+//            print("\(k)       \(z)")
+//        }
+//    }
+//    
+//    func sum(one: Int, two:Int) -> Int {
+//        return one + two
+//    }
+//    
+//    func sum2(_ x:Int, _ y:Int) -> Int {
+//        for _ in 0..<10 {
+//            print("你好")
+//        }
+//        return x+y
+//    }
 }
 
